ddd7cc2c000b906adca2bf2815ca80fc42c0c330
thelovemsg <thelovemsg@naver.com>
2023-02-05 22:18:35 +0900

    Spring Boot
    => Containerless WEb Application Architecture
    
    Web Container (Servlet Container)
     & Web Component (Servlet)
    
     기존에 Servlet Container설정이 너무 힘듬 => Spring Boot 가 해결
    
     스프링 부트를 이용한 개발 방법
    
        부트가 결정한 기술과 구성, 디폴트 설정을 수용
    
        외부 설정 파일을 이용한 설정 변경 방법 활용
    
        아주 빠르게 개발 시작
    
        but...
    
     오해와 한계
    
        애플리케이션 기능 코드만 잘 작성하면 된다.
    
        스프링을 몰라도 개발을 잘할 수 있다.
    
        스프링부트가 직접적으로 보여주지 않은 것은 몰라도 된다.
    
        뭔가 기술적인 필요가 생기면 검색을 해서 해결.
    
    Spring Container의 역할
    
      우리가 메타 정보를 주면 그것을 바탕으로
    
      싱글톤 오브젝트를 만드는데 그게 전부가 아니라 오브젝트가 사용할 다른 의존 오브젝트가 잇  다면 그 오브젝트를 주입해주는 역할까지 해줌.
    
        1. 생성자 주입
    
        2. 팩토리이용
    
        3. setter 메소드 이용 등

---
+++ helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -0,0 +1,15 @@
+package tobyspring.helloboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
+
+public class HelloController {
+
+    public String hello(String name) {
+        SimpleHelloService helloService = new SimpleHelloService();
+        return helloService.sayHello(Objects.requireNonNull(name));
+    }
+
+}
--- helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,13 +1,62 @@
 package tobyspring.helloboot;
 
+import jdk.jfr.ContentType;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
-@SpringBootApplication
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//@SpringBootApplication
 public class HellobootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HellobootApplication.class, args);
+	public static void main(String[] args) throws LifecycleException {
+		GenericApplicationContext applicationContext = new GenericApplicationContext();
+		applicationContext.registerBean(HelloController.class);
+		applicationContext.refresh();
+
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+
+			servletContext.addServlet("hello", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					// 인증, 보안, 다국어, 공통 기능
+					if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())){
+						String name = req.getParameter("name");
+
+						HelloController helloController = applicationContext.getBean(HelloController.class);
+						String ret = helloController.hello(name);
+
+						resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
+						resp.getWriter().println(ret);
+					}else if(req.getRequestURI().equals("/user")){
+						//
+						resp.setStatus(HttpStatus.NOT_FOUND.value());
+					}else {
+
+					}
+				}
+			}).addMapping("/*");
+		});
+		webServer.start();
 	}
 
 }
+
+
